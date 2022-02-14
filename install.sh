879a98e0758ed6637ea9a458a630b9120be58239
Hyun Hwang <hyun@caffeinated.codes>
2022-02-13 19:57:18 -0500

    Use more precise names

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-HOME=${HOME:-/home/`whoami`}
-PACKED_HOME=`dirname $0`/home/hyunh
+SYSTEM_ETC="${ETC:-/etc}"
+SYSTEM_HOME="${HOME:-/home/`whoami`}"
+PACKAGE_ROOT="`dirname $0`"
+PACKAGE_ETC="${PACKAGE_ROOT}/etc"
+PACKAGE_HOME="${PACKAGE_ROOT}/home/hyunh"
 
 # arg 1: string: file to rename, cannot be empty
 # arg 2: string: suffix, defaults to `.old`
@@ -49,7 +52,7 @@ if [ `id -u` -eq 0 ]; then
 fi
 
 # Prevent unnecessary reruns
-if [ -e "${HOME}/.hhh_configs_installed" ]; then
+if [ -e "${SYSTEM_HOME}/.hhh_configs_installed" ]; then
     echo "already installed."
     exit 2
 fi
@@ -58,47 +61,48 @@ fi
 
 echo "Backup some of the existing stuff:"
 # .bashrc
-backup_and_report "${HOME}/.bashrc" .bak true
+backup_and_report "${SYSTEM_HOME}/.bashrc" .bak true
 # .config/konsolerc => on hold, seems like konsole ignores this somehow???
 # .gnupg
-backup_and_report "${HOME}/.gnupg"
+backup_and_report "${SYSTEM_HOME}/.gnupg"
 # .local/share/konsole/Hyun's.profile
-backup_and_report "${HOME}/.local/share/konsole/Hyun's.profile"
+backup_and_report "${SYSTEM_HOME}/.local/share/konsole/Hyun's.profile"
 # .p10k.zsh
-backup_and_report "${HOME}/.p10k.zsh"
+backup_and_report "${SYSTEM_HOME}/.p10k.zsh"
 # .ssh/
-backup_and_report "${HOME}/.ssh"
+backup_and_report "${SYSTEM_HOME}/.ssh"
 # .zshrc
-backup_and_report "${HOME}/.zshrc"
+backup_and_report "${SYSTEM_HOME}/.zshrc"
 
 echo "Install new stuff:"
 # .bashrc
-cat "${PACKED_HOME}/.bashrc.addendum" >> "${HOME}/.bashrc"
-echo "* updated: ${HOME}/.bashrc"
+cat "${PACKAGE_HOME}/.bashrc.addendum" >> "${SYSTEM_HOME}/.bashrc"
+echo "* updated: ${SYSTEM_HOME}/.bashrc"
 # .config/konsolerc => on hold, seems like konsole ignores this somehow???
 # .gnupg
 ## Recommended permissions
-install_and_report "${PACKED_HOME}" "${HOME}" .gnupg true
-chmod 700 "${HOME}/.gnupg"
-chmod 600 "${HOME}/.gnupg/*.conf"
+install_and_report "${PACKAGE_HOME}" "${SYSTEM_HOME}" .gnupg true
+chmod 700 "${SYSTEM_HOME}/.gnupg"
+chmod 600 "${SYSTEM_HOME}/.gnupg/*.conf"
 ## Keyring is probably empty, so let's initialize it with my key
 curl --doh-url https://dns.quad9.net/dns-query --fail --location --silent \
-     https://gitlab.com/hyunh.gpg \
+    https://gitlab.com/hyunh.gpg \
 | gpg --import &> /dev/null
 # .local/share/konsole/Hyun's.profile
-install_and_report "${PACKED_HOME}" "${HOME}" ".local/share/konsole/Hyun's.profile"
+install_and_report "${PACKAGE_HOME}" "${SYSTEM_HOME}" \
+    ".local/share/konsole/Hyun's.profile"
 # .p10k.zsh
-install_and_report "${PACKED_HOME}" "${HOME}" .p10k.zsh
+install_and_report "${PACKAGE_HOME}" "${SYSTEM_HOME}" .p10k.zsh
 # .ssh/
 ## Recommended permissions
-chmod 700 "${PACKED_HOME}/.ssh"
-chmod 600 "${PACKED_HOME}/.ssh/config"
+chmod 700 "${PACKAGE_HOME}/.ssh"
+chmod 600 "${PACKAGE_HOME}/.ssh/config"
 ### Note: Even though this is a public key, ssh REQUIRES it to be treated as
 ###       sensitive info since it is used in ssh publickey authentication.
-chmod 600 "${PACKED_HOME}/.ssh/house.hwang.keys.pgp-ssh@hyun+public.txt"
-install_and_report "${PACKED_HOME}" "${HOME}" .ssh
+chmod 600 "${PACKAGE_HOME}/.ssh/house.hwang.keys.pgp-ssh@hyun+public.txt"
+install_and_report "${PACKAGE_HOME}" "${SYSTEM_HOME}" .ssh
 # .zshrc
-install_and_report "${PACKED_HOME}" "${HOME}" .zshrc
+install_and_report "${PACKAGE_HOME}" "${SYSTEM_HOME}" .zshrc
 
 # TODO: Integrate oh-my-zsh installation with --unattended --keep-zshrc
 # TODO: Install plugins:
@@ -109,4 +113,4 @@ install_and_report "${PACKED_HOME}" "${HOME}" .zshrc
 # TODO: Integrate powerlevel10k theme installation
 
 # Mark installation done
-touch "${HOME}/.hhh_configs_installed"
+touch "${SYSTEM_HOME}/.hhh_configs_installed"
